6a84a8f96a63919aaaabfc72269c8af6cdb02999
dev <developer@innosoft.com.tw>
2023-05-30 17:37:31 +0800

    安裝套件using Microsoft.EntityFrameworkCore; 引用AddDbContext

--- InnoPayApi/Program.cs
+++ InnoPayApi/Program.cs
@@ -1,3 +1,8 @@
+using InnoPayApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore.Design;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("ArasContext");
+builder.Services.AddDbContext<ArasContext>(options =>options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
