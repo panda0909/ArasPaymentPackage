81ee05091ac526c81d459a623f678644cb29ca48
dev <developer@innosoft.com.tw>
2023-05-30 17:22:57 +0800

    將ArasContext.OnModelCreating方法內的連線字串加入至appsettings.json中

--- InnoPayApi/Models/ArasContext.cs
+++ InnoPayApi/Models/ArasContext.cs
@@ -828,8 +828,9 @@ public partial class ArasContext : DbContext
     public virtual DbSet<Wstypeproperty> Wstypeproperties { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=InnovatorSolutions;TrustServerCertificate=True; User ID=innovator;Password=innovator");
+    {
+
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
